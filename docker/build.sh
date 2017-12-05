b7d243e97cc4a5c54cc4fb35b1aead0509701626
Larry <larryrau@mac.com>
2017-12-05 14:24:17 -0500

    make builder image builds and push separate steps; some cleanup of gitignore files

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+#
+# Create the builders
+#
 DOCKER_DIR=$ROOT/docker
 
-cd $DOCKER_DIR/golang-builder && make build_go && make push_go && cd $DOCKER_DIR
-cd $DOCKER_DIR/jq && make build_jq && make push_jq && cd $DOCKER_DIR
-cd $DOCKER_DIR/npm-builder && make build_npm && make push_npm && cd $DOCKER_DIR
-cd $DOCKER_DIR/logger && make compile && make bimage && make uimage && make bpush && make upush && cd $DOCKER_DIR
-cd $DOCKER_DIR/java-builder/oracle-java-8 && make jimage && make jpush && cd $DOCKER_DIR
+cd $DOCKER_DIR/golang-builder && make build_go && cd $DOCKER_DIR
+cd $DOCKER_DIR/jq && make build_jq && cd $DOCKER_DIR
+cd $DOCKER_DIR/npm-builder && make build_npm && cd $DOCKER_DIR
+cd $DOCKER_DIR/logger && make compile && make bimage && make uimage && cd $DOCKER_DIR
+cd $DOCKER_DIR/java-builder/oracle-java-8 && make jimage && cd $DOCKER_DIR
